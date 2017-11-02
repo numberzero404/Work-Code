c3e5337bcc161b9d8965506d96cbb8cb6251cb07
numberzero404 <33104546+numberzero404@users.noreply.github.com>
2017-11-02 13:43:40 -0600

    Update Workflow.R

--- Workflow.R
+++ Workflow.R
@@ -5,10 +5,10 @@
 # -------------------------------------------------------------------------------------------
 
 ##Set this to 3 if you're running it on a Monday, 4 if on a Tuesday, etc
-DAYS <- 3
+DAYS <- 4
 
 #set directory and create file list, directory should be folder with rasch output files in it
-setwd("C:/Users/kcm35/Dropbox/Reader Dashboard & Analysis/rasch output files")
+setwd("C:/Users/kcm35/Dropbox/Projects/2017/Training Dashboard Materials/Rasch output files")
 file.list<- list.files()
 library(xlsx)
 
@@ -30,9 +30,9 @@ rm(dateorder)
 #Reading file just assigned as most recent into a data frame, etc
 
 #Reading in, cleaning file, naming variables
-x <- read.table(newunexpected, fill = T, col.names=c("1","2","3","4","5","6","7","8","9","10","11","12","13", "14"), stringsAsFactors = FALSE)
+x <- read.table(newunexpected, fill = T)
 x<- x[-c(1:3),-c(1,7)]
-colnames(x) <- c("Cat","Score","Exp.","Resd","StRes","N","Raters","Nu1","Ap","Nu2","Tra", "Empty")
+colnames(x) <- c("Cat","Score","Exp.","Resd","StRes","N","Raters","Nu","Ap","Nu","Tra")
   
 #Replacing blanks with NA 
 for(c in 1:ncol(x)){
@@ -42,20 +42,8 @@ for(c in 1:ncol(x)){
 #Only keeping rows with complete data (no NA's)
 x<- x[which(complete.cases(x)),]
 #Removing the first and the last row from the data
-#x<- x[-c(1,nrow(x)),]
-
-
-#shift over columns with last names
-for (i in which((x$Empty)!="|"))
-{
-  x$Raters[i] <- paste(x$Raters[i], x$Nu1[i])
-  x$Nu1[i] <- x$Ap[i]
-  x$Ap[i] <- x$Nu2[i]
-  x$Nu2[i] <- x$Tra[i]
-  x$Tra[i] <- x$Empty[i]
-}
-x <- x[,-12]
-
+x<- x[-c(1,nrow(x)),]
+  
 district <- NULL
 rater <- NULL
   
@@ -72,8 +60,8 @@ x$District <- district
 x$Raters <- rater
   
 #Adding date and week columns to match date of server files
-x$Date <- rep(as.Date(file.info(newunexpected)$ctime)-DAYS, nrow(x))
-x$Week <- rep(as.Date(file.info(newunexpected)$ctime)-(DAYS+4), nrow(x))
+#x$Date <- rep(as.Date(file.info(newunexpected)$ctime)-DAYS, nrow(x))
+#x$Week <- rep(as.Date(file.info(newunexpected)$ctime)-(DAYS+4), nrow(x))
 
 #Creating data frame to keep using file name 
 unexpected.output <- x
@@ -82,7 +70,7 @@ unexpected.output <- x
 remove(x, district, rater)
 
 #Append new data onto final Output data set
-write.table(unexpected.output, "C:/Users/kcm35/Dropbox/Reader Dashboard & Analysis/final weekly dashboard files/Unexpected Output.csv", append=TRUE, row.names = F, col.names=FALSE, sep=",", na = "")
+write.table(unexpected.output, "C:/Users/kcm35/Dropbox/Projects/2017/Training Dashboard Materials/Final dashboard files/Unexpected Output.csv", append=TRUE, row.names = F, col.names=F, sep=",", na = "")
 rm(unexpected.output, i, j, unexpected.output.names)
 
 # -------------------------------------------------------------------------------------------
@@ -104,33 +92,33 @@ rm(dateorder)
 #Reading "raters.district" files into R
 
 #Reading in the file - This is a temp data frame
-x <- read.xlsx(newraters, 1, startRow=2, header=TRUE, stringsAsFactors=FALSE)
+x <- read.csv(newraters, skip=1, header=TRUE, stringsAsFactors=FALSE)
   
-district <- NULL
+group <- NULL
 rater <- NULL
   
 #Split reader name on period on assign to two different variables
 for (j in 1:nrow(x)){
   rd <- unlist((strsplit(toString(x$Raters[j]), "[.]")))
-  district <- c(district, rd[1])
+  group <- c(group, rd[1])
   rater <- c(rater, rd[2])
 }
 
 x$Raters <- rater
-x$District <- district
+x$Group <- group
   
 #Adding date and week column to match date of server files
-x$Date <- rep(as.Date(file.info(newraters)$ctime)-DAYS, nrow(x))
-x$Week <- rep(as.Date(file.info(newraters)$ctime)-(DAYS+4), nrow(x))
+#x$Date <- rep(as.Date(file.info(newraters)$ctime)-DAYS, nrow(x))
+#x$Week <- rep(as.Date(file.info(newraters)$ctime)-(DAYS+4), nrow(x))
   
 #Creating data frame to keep using file name 
 rater.output <- x
   
 #Removing the temp data frame from the working environment
-remove(x, rd, district, rater)
+remove(x, rd, group, rater)
 
 #Append rater data onto existing final output file
-write.table(rater.output, "C:/Users/kcm35/Dropbox/Reader Dashboard & Analysis/final weekly dashboard files/Rater Output.csv", append=TRUE, row.names = F, col.names=FALSE, sep=",", na = "")
+write.table(rater.output, "C:/Users/kcm35/Dropbox/Projects/2017/Training Dashboard Materials/Final dashboard files/Rater Output.csv", append=TRUE, row.names = F, col.names=F, sep=",", na = "")
 rm(rater.output.all, i, j, rater.output.names)
 
 # -------------------------------------------------------------------------------------------
@@ -160,8 +148,8 @@ graph.output <- melt(x, id.vars = c("Scale", "Measure", "Expected", "ExpCat"))
 colnames(graph.output) <- c("Scale", "Measure", "Expected", "ExpCat", "Prob.Name", "Prob.Value")
 
 #Adding date and week column to match date of server files
-graph.output$Date <- rep(as.Date(file.info(newgraph)$ctime)-DAYS, nrow(graph.output))
-graph.output$Week <- rep(as.Date(file.info(newgraph)$ctime)-(DAYS+4), nrow(graph.output))
+#graph.output$Date <- rep(as.Date(file.info(newgraph)$ctime)-DAYS, nrow(graph.output))
+#graph.output$Week <- rep(as.Date(file.info(newgraph)$ctime)-(DAYS+4), nrow(graph.output))
 
 #Append to graph output file
-write.table(graph.output, "C:/Users/kcm35/Dropbox/Reader Dashboard & Analysis/final weekly dashboard files/Graph Output.csv", append=TRUE, row.names = F, col.names=FALSE, sep=",", na = "")
+write.table(graph.output, "C:/Users/kcm35/Dropbox/Projects/2017/Training Dashboard Materials/Final dashboard files/Graph Output.csv", append=F, row.names = F, col.names=T, sep=",", na = "")
